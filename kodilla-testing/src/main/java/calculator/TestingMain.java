f608ccf862a2435ba79f857f585bea4913f2b812
Lukasz Banas <lukasz.banas345@gmail.com>
2022-04-10 12:10:58 +0200

    Properties

--- kodilla-testing/src/main/java/calculator/TestingMain.java
+++ kodilla-testing/src/main/java/calculator/TestingMain.java
@@ -3,27 +3,18 @@ package calculator;
 public class TestingMain {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.sum();
-        calculator.sub();
-        if (calculator.sum() == 15) {
+        int calcAdd =  calculator.add(8,7);
+        int calcSub = calculator.substract(8,7);
+        if (calcAdd == 15) {
             System.out.println("test OK");
         } else {
             System.out.println("Error!");
         }
-        if (calculator.sub() == 1) {
+        if (calcSub == 1) {
             System.out.println("test OK");
         } else {
             System.out.println("Error!");
         }
     }
 }
-class Calculator{
-        int a = 8;
-        int b = 7;
-        public int sum(){
-            return a+b;
-        }
-        public int sub(){
-            return a-b;
-        }
-}
+
