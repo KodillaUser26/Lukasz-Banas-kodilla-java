396f325a59040b3389efc11075df7472d85ba230
Lukasz Banas <lukasz.banas345@gmail.com>
2022-08-02 15:49:02 +0200

    15.1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,21 @@
+package com.kodilla.patterns.singleton;
+
+import com.com.kodilla.patterns.singleton.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LoggerTestSuite {
+
+
+    @Test
+    void getLastLog() {
+        //given
+        Logger logger = Logger.INSTANCE;
+        //when
+        String lastLog = "123";
+        logger.log(lastLog);
+        //then
+        Assertions.assertEquals(logger.getLastLog(), lastLog);
+
+    }
+}
