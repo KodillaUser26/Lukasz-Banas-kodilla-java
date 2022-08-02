5504e4232e0f0c3dc2388ee870f353abbf3cbc5c
Lukasz Banas <lukasz.banas345@gmail.com>
2022-08-02 16:45:11 +0200

    15.2

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -0,0 +1,42 @@
+package com.kodilla.patterns.prototype.library;
+
+import com.kodilla.patterns.prototype.Prototype;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Library extends Prototype<Library> {
+    String name;
+    Set<Book> books = new HashSet<>();
+
+    public Library(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public String setName (String newName){
+        return name = newName;
+    }
+
+    public Library shallowCopy () throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public Library deepCopy () throws  CloneNotSupportedException{
+        Library clonedLibrary = super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for (Book bookSet : books) {
+            Book clonedBook = new Book(bookSet.getTitle(), bookSet.getAuthor(), bookSet.getPublicationDate());
+            clonedLibrary.books.add(clonedBook);
+        }
+        return clonedLibrary;
+    }
+}
